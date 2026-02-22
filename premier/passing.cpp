e7e440dcf336b26e529a22c78b8ba3b063787f22
faaizxnova <faaizxnova@gmail.com>
2026-02-23 00:15:42 +0530

    Modified file passing.cpp

--- premier/passing.cpp
+++ premier/passing.cpp
@@ -8,12 +8,28 @@ void display(const int * begin, const int * end) {
         cout << *begin << " ";
     }
 }
-int main(void) {
+int main(int argc, char ** argv) {
     ios :: sync_with_stdio(false);
-    int arr[] {1, 2, 3, 4};
-    
+    // int arr[] {1, 2, 3, 4};
     // const int * ptr = arr;
-    display(arr, arr + 4);
+    // display(arr, arr + 4);
     // cout << end(arr) - begin(arr);
+    // int * arr[2];
+    // for(int i = 0; i < 2; ++ i) {
+    //     int sub[2];
+    //     for(int j = 0; j < 2; ++ j) {
+    //         cout << "Enter elements : "; cin >> sub[j];
+    //     }
+    //     arr[i] = sub;
+    // }
+
+    // for (int i = 0; i < 2; ++i) {
+    //     for (int j = 0; j < 2; ++j) {
+    //         cout << *arr[i] ++;
+    //     }
+    //     cout << '\n';
+    // }
+
+    cout << atoi(argv[1]) + atoi(argv[2]);
     return 0;
 }
